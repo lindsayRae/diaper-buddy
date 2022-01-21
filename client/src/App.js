4b1776a57c3a62e1963603f377ce11fad29bd759
Lindsay <lbarnett712@gmail.com>
2022-01-21 07:47:43 -0500

    test: routing

--- client/src/App.js
+++ client/src/App.js
@@ -15,12 +15,30 @@ const usePosts = () => {
   }, []);
   return post;
 };
+
+const usePosts2 = () => {
+  const [post2, setPosts2] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts2 = async () => {
+      const res2 = await fetch('/api');
+      const data2 = await res2.json();
+      setPosts2(data2);
+    };
+    fetchPosts2();
+  }, []);
+  return post2;
+};
 function App() {
   const posts = usePosts();
+  const posts2 = usePosts2();
   return (
     <div className='App'>
       {posts.map((post) => (
         <h2>{post.title}</h2>
+      ))}
+      {posts2.map((post2) => (
+        <p>{post2.title}</p>
       ))}
     </div>
   );

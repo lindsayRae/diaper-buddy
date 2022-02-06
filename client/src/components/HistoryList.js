84dd76351f6e9f15cfc3bff94e58d7f4ea767bdd
Lindsay <lbarnett712@gmail.com>
2022-02-06 12:02:25 -0500

    feat: modal component with transitions

--- client/src/components/HistoryList.js
+++ client/src/components/HistoryList.js
@@ -1,75 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './HistoryGraphs.css';
+import Modal from './Modal/Modal';
+
 const HistoryList = () => {
+  const [modalVisible, setModalVisible] = useState(false);
+
   return (
-    <section className='section history-card'>
-      <ul className='list history-list'>
-        <li className='history-list-item'>
-          <div>
-            <span className='list-date'>01/10/22</span>
-            <span className='list-count'>10 used</span>
-          </div>
-          <button className='btn-link'>Edit</button>
-        </li>
-        <li className='history-list-item'>
-          <div>
-            <span className='list-date'>01/10/22</span>
-            <span className='list-count'>10 used</span>
-          </div>
-          <button className='btn-link'>Edit</button>
-        </li>
-        <li className='history-list-item'>
-          <div>
-            <span className='list-date'>01/10/22</span>
-            <span className='list-count'>10 used</span>
-          </div>
-          <button className='btn-link'>Edit</button>
-        </li>
-        <li className='history-list-item'>
-          <div>
-            <span className='list-date'>01/10/22</span>
-            <span className='list-count'>10 used</span>
-          </div>
-          <button className='btn-link'>Edit</button>
-        </li>
-        <li className='history-list-item'>
-          <div>
-            <span className='list-date'>01/10/22</span>
-            <span className='list-count'>10 used</span>
-          </div>
-          <button className='btn-link'>Edit</button>
-        </li>
-        <li className='history-list-item'>
-          <div>
-            <span className='list-date'>09/10/22</span>
-            <span className='list-count'>10 used</span>
-          </div>
-          <button className='btn-link'>Edit</button>
-        </li>
-        <li className='history-list-item'>
-          <div>
-            <span className='list-date'>09/10/22</span>
-            <span className='list-count'>10 used</span>
-          </div>
-          <button className='btn-link'>Edit</button>
-        </li>
-        <li className='history-list-item'>
-          <div>
-            <span className='list-date'>09/10/22</span>
-            <span className='list-count'>10 used</span>
-          </div>
-          <button className='btn-link'>Edit</button>
-        </li>
-        <li className='history-list-item'>
-          <div>
-            <span className='list-date'>09/10/22</span>
-            <span className='list-count'>10 used</span>
-          </div>
-          <button className='btn-link'>Edit</button>
-        </li>
-      </ul>
-    </section>
+    <>
+      <section className='section history-card'>
+        <ul className='list history-list'>
+          <li className='history-list-item'>
+            <div>
+              <span className='list-date'>01/10/22</span>
+              <span className='list-count'>10 used</span>
+            </div>
+            <button className='btn-link' onClick={() => setModalVisible(true)}>
+              Edit
+            </button>
+          </li>
+        </ul>
+      </section>
+      <Modal open={modalVisible} closeModal={() => setModalVisible(false)}>
+        <>
+          <h2>Hello from the modal</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis,
+            recusandae.
+          </p>
+        </>
+      </Modal>
+    </>
   );
 };
 
---
+++ client/src/components/Modal/Modal.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './Modal.css';
+import { CSSTransition } from 'react-transition-group';
+
+const modalRoot = document.querySelector('#modal-root');
+
+const Modal = ({ open, closeModal, children }) => {
+  console.log(open);
+  const handleClick = (e) => {
+    if (e.target.id === 'modal-overlay') {
+      closeModal();
+    }
+  };
+
+  const modal = (
+    <CSSTransition in={open} timeout={500} classNames='show-up' unmountOnExit>
+      <div className='modal-overlay' id='modal-overlay' onClick={handleClick}>
+        <div className='modal'>{children}</div>
+      </div>
+    </CSSTransition>
+  );
+
+  return ReactDOM.createPortal(modal, modalRoot);
+};
+
+export default Modal;

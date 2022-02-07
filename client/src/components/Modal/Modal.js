a6c2d420016dbb4fb2032b7985cca7175224ea28
Lindsay <lbarnett712@gmail.com>
2022-02-07 07:02:04 -0500

    fix: useRef

--- client/src/components/Modal/Modal.js
+++ client/src/components/Modal/Modal.js
@@ -7,6 +7,7 @@ const modalRoot = document.querySelector('#modal-root');
 
 const Modal = ({ open, closeModal, children }) => {
   console.log(open);
+  const nodeRef = React.useRef(null);
   const handleClick = (e) => {
     if (e.target.id === 'modal-overlay') {
       closeModal();
@@ -14,8 +15,19 @@ const Modal = ({ open, closeModal, children }) => {
   };
 
   const modal = (
-    <CSSTransition in={open} timeout={500} classNames='show-up' unmountOnExit>
-      <div className='modal-overlay' id='modal-overlay' onClick={handleClick}>
+    <CSSTransition
+      in={open}
+      timeout={500}
+      classNames='show-up'
+      nodeRef={nodeRef}
+      unmountOnExit
+    >
+      <div
+        ref={nodeRef}
+        className='modal-overlay'
+        id='modal-overlay'
+        onClick={handleClick}
+      >
         <div className='modal'>{children}</div>
       </div>
     </CSSTransition>

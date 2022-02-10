9e5107ae90d4ea1b9ddf478cb27a86858a49cbd6
Lindsay <lbarnett712@gmail.com>
2022-02-10 14:38:17 -0500

    fix: layout

--- client/src/components/CardHeading.js
+++ client/src/components/CardHeading.js
@@ -5,11 +5,11 @@ const CardHeading = (props) => {
   let size = props.currentSize;
   return (
     <div className='full-card'>
-      <div className='diaper-ct'>{size.count}</div>
-      <h2>
-        {size.heading} Diapers <br />
-        on hand
-      </h2>
+      <div className='card-count-text'>
+        <div className='diaper-ct'>{size.count}</div>
+        <h2>{size.heading} Diapers on hand</h2>
+      </div>
+
       <img src={vector3} alt='vector3' className='img-absolute' />
     </div>
   );

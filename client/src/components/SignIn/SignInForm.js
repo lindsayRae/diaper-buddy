45691783ab4bee8a23e236c41647d3e49e2d0315
Lindsay <lbarnett712@gmail.com>
2022-02-27 07:37:23 -0500

    feat: login functionality

--- client/src/components/SignIn/SignInForm.js
+++ client/src/components/SignIn/SignInForm.js
@@ -1,22 +1,83 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { useState, useContext } from 'react';
+
+import { NavLink, useNavigate } from 'react-router-dom';
+import { UserContext } from '../../context/UserContext';
 import '../../components/Inputs.css';
 import './SignIn.css';
 
-const SignInForm = ({ history }) => {
+const SignInForm = () => {
+  let navigate = useNavigate();
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const { setUser } = useContext(UserContext);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    if (!email || !password) {
+      setError('You must provide an email and password.');
+      return;
+    }
+    try {
+      const response = await fetch(`/api/auth`, {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify({
+          email,
+          password,
+        }),
+      });
+
+      const data = await response.json();
+
+      if (data.message) {
+        setError(data.message);
+        return;
+      }
+
+      setUser(data);
+      localStorage.setItem('userData', JSON.stringify(data));
+      navigate('/home');
+    } catch (err) {
+      setError(`Something went wrong: ${err}`);
+    }
+  };
   return (
     <div className='block' style={{ paddingTop: '0' }}>
-      <form>
-        <input className='input' placeholder='Email' />
+      <form onSubmit={handleSubmit}>
+        <input
+          className='input'
+          placeholder='Email'
+          type='text'
+          name='email'
+          value={email}
+          onChange={(event) => {
+            setError('');
+            setEmail(event.target.value);
+          }}
+          autoComplete='off'
+        />
         <div className='input-password'>
-          <input className='input' placeholder='Password' />
-          {/* <span className='show'>
-            <a>Show</a>
-          </span> */}
+          <input
+            className='input'
+            placeholder='Password'
+            type='password'
+            name='password'
+            value={password}
+            onChange={(event) => {
+              setError('');
+              setPassword(event.target.value);
+            }}
+            autoComplete='off'
+          />
         </div>
-        <NavLink to='/home'>
-          <button className='btn btn-lt'>Sign In</button>
-        </NavLink>
+        {error && <p className='error-msg'>{error}</p>}
+        <button className='btn btn-lt'>Sign In</button>
       </form>
       <p className='forgot'>
         <NavLink to='/passwordreset'>Forgot Password</NavLink>

7123c972db8240e184a11d09536de59db797cfbf
Lindsay <lbarnett712@gmail.com>
2022-03-31 07:36:21 -0400

    chore: switched back to db data

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,6 @@ const _ = require('lodash');
 const { User, validateLogin } = require('../models/user.model');
 const express = require('express');
 const router = express.Router();
-const users = require('../models/user.json');
 
 /**
  * @description login a user
@@ -15,23 +14,23 @@ router.post('/', async (req, res) => {
 
   let email = req.body.email;
 
-  //let user = await User.findOne({ email: email.toLowerCase() });
-  let user = users.find((o) => o.email === email);
+  let user = await User.findOne({ email: email.toLowerCase() });
+
   console.log('user', user);
   if (!user) {
     return res.status(400).send({ message: 'Invalid email or password.' });
   }
 
-  // const validPassword = await bcrypt.compare(req.body.password, user.password);
+  const validPassword = await bcrypt.compare(req.body.password, user.password);
 
-  // if (!validPassword) {
-  //   return res.status(400).send({ message: 'Invalid email or password.' });
-  // }
+  if (!validPassword) {
+    return res.status(400).send({ message: 'Invalid email or password.' });
+  }
 
-  // const token = user.generateAuthToken();
-  // console.log('token', token);
+  const token = user.generateAuthToken();
+  console.log('token', token);
 
-  // res.send({ jwt: token, user: _.pick(user, ['firstName', 'email', '_id']) });
+  //res.send({ jwt: token, user: _.pick(user, ['firstName', 'email', '_id']) });
   res.send({
     jwt: 'qwertasdf12346',
     user: user,

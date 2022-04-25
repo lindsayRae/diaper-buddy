555eed6eca97a5e1e59e46b4409e4810575ca305
Lindsay <lbarnett712@gmail.com>
2022-04-25 06:45:11 -0400

    feat: added display conditionals

--- client/src/components/HistoryGraph.js
+++ client/src/components/HistoryGraph.js
@@ -11,27 +11,35 @@ import {
 const HistoryGraph = ({ history }) => {
   return (
     <section className='section history-card'>
-      <ResponsiveContainer width='100%' height='100%'>
-        <BarChart
-          width={150}
-          height={200}
-          data={history}
-          margin={{
-            top: 5,
-            right: 0,
-            left: -40,
-            bottom: 0,
-          }}
-        >
-          <XAxis dataKey='date' />
-          <YAxis />
-          {history.length > 6 && (
-            <Brush dataKey='date' height={20} stroke='#3F9AE0' endIndex={6} />
-          )}
+      {history.length === 0 && (
+        <p>
+          No used diaper history yet. Remove a diaper from above to start
+          tracking!
+        </p>
+      )}
+      {history.length > 0 && (
+        <ResponsiveContainer width='100%' height='100%'>
+          <BarChart
+            width={150}
+            height={200}
+            data={history}
+            margin={{
+              top: 5,
+              right: 0,
+              left: -40,
+              bottom: 0,
+            }}
+          >
+            <XAxis dataKey='date' />
+            <YAxis />
+            {history.length > 6 && (
+              <Brush dataKey='date' height={20} stroke='#3F9AE0' endIndex={6} />
+            )}
 
-          <Bar dataKey='amount' fill='#3F9AE0' />
-        </BarChart>
-      </ResponsiveContainer>
+            <Bar dataKey='amount' fill='#3F9AE0' />
+          </BarChart>
+        </ResponsiveContainer>
+      )}
     </section>
   );
 };
--- client/src/components/HistoryList.js
+++ client/src/components/HistoryList.js
@@ -12,26 +12,33 @@ const HistoryList = ({ history }) => {
     <>
       <section className='section history-card'>
         <ul className='list history-list'>
-          {history.map((item, index) => {
-            return (
-              <li className='history-list-item' key={index}>
-                <div>
-                  <span className='list-date'>{item.date}</span>
-                  <span className='list-count'>{item.amount} used</span>
-                </div>
-                <button
-                  className='btn-link'
-                  onClick={() => {
-                    setModalVisible(true);
-                    setModalData(item);
-                  }}
-                  style={{ color: '#3f9ae0' }}
-                >
-                  Edit
-                </button>
-              </li>
-            );
-          })}
+          {!history && (
+            <p>
+              No used diaper history yet. Remove a diaper from above to start
+              tracking!
+            </p>
+          )}
+          {history &&
+            history.map((item, index) => {
+              return (
+                <li className='history-list-item' key={index}>
+                  <div>
+                    <span className='list-date'>{item.date}</span>
+                    <span className='list-count'>{item.amount} used</span>
+                  </div>
+                  <button
+                    className='btn-link'
+                    onClick={() => {
+                      setModalVisible(true);
+                      setModalData(item);
+                    }}
+                    style={{ color: '#3f9ae0' }}
+                  >
+                    Edit
+                  </button>
+                </li>
+              );
+            })}
         </ul>
       </section>
       <Modal open={modalVisible} closeModal={() => setModalVisible(false)}>

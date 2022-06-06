6e58600f04b08ad4a0b94af37a8d951ea7159c53
Lindsay <lbarnett712@gmail.com>
2022-06-06 07:12:12 -0400

    feat: select ddls, fixed current child data flow

--- client/src/pages/Settings.js
+++ client/src/pages/Settings.js
@@ -1,64 +1,113 @@
 import React, { useState, useContext, useEffect } from 'react';
+import Select from 'react-select';
+import { UserContext } from '../context/UserContext';
+import { useNavigate } from 'react-router-dom';
 import Modal from '../components/Modal/Modal';
 import AddChild from '../components/AddChild';
 import Logout from '../components/Logout';
 import Navbar from '../components/Nav/Navbar';
-import { UserContext } from '../context/UserContext';
-import { useNavigate } from 'react-router-dom';
+import Toast from '../components/Toast/Toast';
+
 import icon from '../images/icon.png';
 import { FaBaby } from 'react-icons/fa';
 import { AiOutlineShoppingCart, AiOutlineBell } from 'react-icons/ai';
 import { TiSortNumerically } from 'react-icons/ti';
 import './Settings.css';
 
+const brandList = [
+  { value: 'Huggies', label: 'Huggies' },
+  { value: 'Kirkland', label: 'Kirkland' },
+  { value: 'Pampers', label: 'Pampers' },
+  { value: "Parents's Choice", label: "Parent's Choice" },
+  { value: 'Up & Up', label: 'Up & Up' },
+];
+const sizeList = [
+  { value: 'Newborn', label: 'Newborn' },
+  { value: 'Size 1', label: 'Size 1' },
+  { value: 'Size 2', label: 'Size 2' },
+  { value: 'Size 3', label: 'Size 3' },
+  { value: 'Size 4', label: 'Size 4' },
+];
+const alertList = [
+  { value: 'None', label: 'None' },
+  { value: '10', label: '10' },
+  { value: '20', label: '20' },
+  { value: '50', label: '50' },
+  { value: '100', label: '100' },
+];
+
 const Settings = () => {
-  const [baby, setBaby] = useState('');
-  const [babyID, setBabyID] = useState('');
-  const [brand, setBrand] = useState('');
-  const [currentSize, setCurrentSize] = useState('');
-  const [currentSizeLabel, setCurrentSizeLabel] = useState('');
-  const [lowAlert, setLowAlert] = useState('');
-  const [modalVisible, setModalVisible] = useState(false);
-  const [modalData, setModalData] = useState();
-  const [isAddChild, setIsAddChild] = useState(false);
-  const [oneKid, setOneKid] = useState(true);
-  const [error, setError] = useState('');
   const { user, setUser } = useContext(UserContext);
   const isAuthenticated = localStorage.getItem('userData');
   let navigate = useNavigate();
-  console.log(user);
-  useEffect(async () => {
-    if (!isAuthenticated) {
-      navigate('/');
-    }
-    console.log('in use effect');
-    if (!modalVisible) {
-      console.log('get current child');
 
-      setPage();
-    }
-  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+  const [kidsData, setKidsData] = useState([]);
 
-  const setPage = async () => {
-    let kids = await getKids();
-    console.log(kids);
-    if (!kids || kids.length === 0) return openAddChild();
-    if (kids.length > 1) {
-      setOneKid(false);
-    } else {
-      setBaby(kids[0].firstName);
-      setBabyID(kids[0]._id);
-      setBrand(kids[0].brandPreference);
-      setCurrentSize(kids[0].currentSize);
-      setLowAlert(kids[0].lowAlert);
-    }
-    // set select option to current child
+  const [babyName, setBabyName] = useState('');
+  const [nameList, setNameList] = useState([]);
+  const [nameOption, setNameOption] = useState();
+  const [brandOption, setBrandOption] = useState();
+  const [sizeOption, setSizeOption] = useState();
+  const [alertOption, setAlertOption] = useState();
+
+  const [babyID, setBabyID] = useState('');
+
+  const [modalVisible, setModalVisible] = useState(false);
+  const [modalData, setModalData] = useState();
+  const [isAddChild, setIsAddChild] = useState(false);
+  const [error, setError] = useState('');
+
+  const [list, setList] = useState([]);
+  let toastProperties = null;
+
+  const setMultiChildren = async (data) => {
+    const nameOptionList = await createNameOptions(data);
+
+    let currentID = user.user.currentChild;
+
+    let currentChildData = data.find((x) => x._id === currentID);
+
+    setNameList(nameOptionList);
+    setBabyID(currentChildData._id);
+    setNameOption({
+      label: currentChildData.firstName,
+      value: currentChildData.firstName,
+    });
+
+    setBrandOption({
+      label: currentChildData.brandPreference,
+      value: currentChildData.brandPreference,
+    });
+    setSizeOption({
+      label: currentChildData.currentSize,
+      value: currentChildData.currentSize,
+    });
+    setAlertOption({
+      label: currentChildData.lowAlert,
+      value: currentChildData.lowAlert,
+    });
+  };
+  const setOneChild = (data) => {
+    setBabyName(data[0].firstName);
+    setBabyID(data[0]._id);
+    setBrandOption({
+      label: data[0].brandPreference,
+      value: data[0].brandPreference,
+    });
+    setSizeOption({
+      label: data[0].currentSize,
+      value: data[0].currentSize,
+    });
+    setAlertOption({
+      label: data[0].lowAlert,
+      value: data[0].lowAlert,
+    });
   };
   const getKids = async () => {
     const user_ID = user.user._id;
-    try {
-      const url = `/api/kids/${user_ID}`;
+    const url = `/api/kids/${user_ID}`;
 
+    try {
       const headers = {
         'Content-Type': 'application/json',
         'x-auth-token': user.jwt,
@@ -68,34 +117,113 @@ const Settings = () => {
         headers: headers,
       });
       const data = await res.json();
-      console.log('getKids data:', data);
+
       if (data.message) {
+        console.log('error', error);
         setError(data.message);
         return;
       } else {
-        return data;
+        if (!data || data.length === 0) return openAddChild();
+        setKidsData(data);
+        if (data.length > 1) {
+          setMultiChildren(data);
+        } else {
+          setOneChild(data);
+        }
       }
     } catch (error) {
+      console.log('error', error);
       setError(error.message);
     }
+  };
+
+  const findNewObj = (obj) => {
+    return kidsData.find((x) => obj === x.firstName);
+  };
+  useEffect(() => {
+    if (!isAuthenticated) {
+      navigate('/');
+    }
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  useEffect(() => {
+    if (!modalVisible) {
+      getKids();
+    }
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  // need to turn and array into a array of objects
+  const createNameOptions = (data) => {
+    let arr = data.map((el) => el.firstName);
+    return arr.map((el) => ({ value: el, label: el }));
   };
 
   const openAddChild = (e) => {
     setIsAddChild(true);
     setModalVisible(true);
   };
+  const showToast = (type, description) => {
+    switch (type) {
+      case 'success':
+        toastProperties = {
+          id: list.length + 1,
+          description: 'Saved',
+          backgroundColor: '#5cb85c',
+        };
+        break;
+      case 'info':
+        toastProperties = {
+          id: list.length + 1,
+          description: description,
+          backgroundColor: '#5bc0de',
+        };
+        break;
+      case 'danger':
+        toastProperties = {
+          id: list.length + 1,
+          description: 'This is a error',
+          backgroundColor: '#d9534f',
+        };
+        break;
+      default:
+        toastProperties = [];
+    }
 
+    setList([...list, toastProperties]);
+  };
+  const updateUserData = async (kid_id) => {
+    const user_id = user.user._id;
+    let body = {
+      kidID: kid_id,
+    };
+    try {
+      const res = await fetch(`/api/users/update/${user_id}`, {
+        method: 'PUT',
+        headers: {
+          'x-auth-token': user.jwt,
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify(body),
+      });
+      const data = await res.json();
+      user.user.currentChild = data.currentChild;
+      setUser(user);
+      localStorage.setItem('userData', JSON.stringify(user));
+      return data;
+    } catch (error) {
+      setError(error.message);
+    }
+  };
   const handleSubmit = async (e) => {
-    console.log('submitting form...');
     e.preventDefault();
+
     let body = {
       user_id: user.user._id,
       _id: babyID,
-      firstName: baby,
-      brandPreference: brand,
-      currentSize: currentSize,
-      currentSizeLabel: currentSizeLabel,
-      lowAlert: lowAlert,
+      firstName: kidsData.length === 1 ? babyName : nameOption.value,
+      brandPreference: brandOption.value,
+      currentSize: sizeOption.value,
+      lowAlert: alertOption.value,
     };
 
     try {
@@ -107,9 +235,17 @@ const Settings = () => {
         },
         body: JSON.stringify(body),
       });
+
       const data = await res.json();
       console.log(data);
+      if (data.message) {
+        setError(data.message);
+        return;
+      }
+
+      showToast('success');
     } catch (error) {
+      console.log(error);
       setError(error.message);
     }
   };
@@ -120,6 +256,7 @@ const Settings = () => {
         <div className='logout'>
           <Logout user={user} setUser={setUser} />
         </div>
+        <Toast toastList={list} position='top-left' setList={setList} />
 
         <div className='setting-heading'>
           <img src={icon} alt='icon' />
@@ -128,11 +265,11 @@ const Settings = () => {
       </section>
       <section className='section' style={{ marginTop: '20px' }}>
         <form onSubmit={handleSubmit}>
-          {oneKid ? (
+          {kidsData.length === 1 ? (
             <div className='input-line-container'>
               <FaBaby
                 size={20}
-                className='edit-count-icon'
+                className='select-icon'
                 style={{
                   marginTop: '8px',
                   width: '24px',
@@ -141,91 +278,92 @@ const Settings = () => {
               <input
                 type='text'
                 className='input-line input-name'
-                value={baby}
+                value={babyName}
                 placeholder='Baby Name'
                 style={{ marginLeft: '5px', paddingBottom: '25px' }}
-                onChange={(e) => setBaby(e.target.value)}
+                onChange={(e) => setBabyName(e.target.value)}
               />
             </div>
           ) : (
             <div className='input-line-container'>
               <FaBaby
                 size={20}
-                className='edit-count-icon'
+                className='select-icon'
                 style={{ marginTop: '6px' }}
               />
-              <select
+              <Select
                 className='settings-select'
-                onChange={(e) => setBrand(e.target.value)}
-                value={brand}
-              >
-                <option>Baby Name</option>
-                <option>test</option>
-              </select>
+                onChange={async (obj) => {
+                  setNameOption({
+                    label: obj.label,
+                    value: obj.value,
+                  });
+                  let newObj = findNewObj(obj.label);
+                  setBabyID(newObj._id);
+                  setBrandOption({
+                    label: newObj.brandPreference,
+                    value: newObj.brandPreference,
+                  });
+                  setSizeOption({
+                    label: newObj.currentSize,
+                    value: newObj.currentSize,
+                  });
+                  setAlertOption({
+                    label: newObj.lowAlert,
+                    value: newObj.lowAlert,
+                  });
+
+                  let userUpdated = await updateUserData(newObj._id);
+                  if (userUpdated)
+                    showToast('info', 'Current child has been updated.');
+                }}
+                options={nameList}
+                value={nameOption}
+              />
             </div>
           )}
 
           <div className='input-line-container'>
             <AiOutlineShoppingCart
               size={24}
-              className='edit-count-icon'
+              className='select-icon'
               style={{ marginTop: '6px' }}
             />
-            <select
+            <Select
               className='settings-select'
-              onChange={(e) => setBrand(e.target.value)}
-              value={brand}
-            >
-              <option value='0'>Brand Preference</option>
-              <option value='huggies'>Huggies</option>
-              <option value='kirkland'>Kirkland</option>
-              <option value='pampers'>Pampers</option>
-              <option value='parents'>Parent's Choice</option>
-              <option value='up'>Up & Up</option>
-            </select>
+              onChange={setBrandOption}
+              options={brandList}
+              value={brandOption}
+              placeholder={'Select Brand Preference'}
+            />
           </div>
           <div className='input-line-container'>
             <TiSortNumerically
               size={24}
-              className='edit-count-icon'
+              className='select-icon'
               style={{ marginTop: '4px' }}
             />
-            <select
+            <Select
               className='settings-select'
-              onChange={(e) => {
-                setCurrentSize(e.target.value);
-                setCurrentSizeLabel(
-                  e.target.options[e.target.selectedIndex].text
-                );
-              }}
-              value={currentSize}
-            >
-              <option value='-1'>Current Size</option>
-              <option value='0'>Newborn</option>
-              <option value='1'>Size 1</option>
-              <option value='2'>Size 2</option>
-              <option value='3'>Size 3</option>
-              <option value='4'>Size 4</option>
-            </select>
+              onChange={setSizeOption}
+              options={sizeList}
+              value={sizeOption}
+              placeholder={'Select Current Size'}
+            />
           </div>
           <div className='input-line-container'>
             <AiOutlineBell
               size={24}
-              className='edit-count-icon'
+              className='select-icon'
               style={{ marginTop: '5px' }}
             />
-            <select
+            <Select
               className='settings-select'
-              onChange={(e) => setLowAlert(e.target.value)}
-              value={lowAlert}
-            >
-              <option value='0'>Low Alert</option>
-              <option value='none'>None</option>
-              <option value='10'>Less than 10</option>
-              <option value='20'>Less than 20</option>
-              <option value='50'>Less than 50</option>
-              <option value='100'>Less than 100</option>
-            </select>
+              onChange={setSizeOption}
+              options={alertList}
+              value={alertOption}
+              placeholder={'Select Low Alert'}
+            />
           </div>
 
           <button className='btn btn-lt settings-btn'>Save</button>
@@ -246,14 +384,18 @@ const Settings = () => {
         {isAddChild && (
           <AddChild
             user={user}
-            modalData={modalData}
+            updateUserData={updateUserData}
             closeModal={() => setModalVisible(false)}
-            setPage={setPage}
+            getKids={getKids}
           />
         )}
       </Modal>
       <Navbar />
-      {error && <p>{error}</p>}
+      {error && (
+        <p style={{ color: '#d9534f', padding: '0 20px', textAlign: 'center' }}>
+          {error}
+        </p>
+      )}
     </div>
   );
 };

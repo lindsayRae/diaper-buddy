7ebbe2f999887717643a7dd370aa0fcc0adeff5e
Lindsay <lbarnett712@gmail.com>
2022-09-27 22:04:37 -0400

    fix : port var

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const port = process.env.PORT || 1234;
+const port = process.env.PORT; // || 1234;
 const app = express();
 const puppeteer = require('puppeteer');
 
@@ -75,15 +75,21 @@ app.listen(port, () => {
 // puppeteer
 // (async () => {
 //   //! change headless to true when finished with code
+//   console.log('running pupperteer...');
 //   const url =
 //     'https://www.target.com/p/huggies-little-snugglers-diapers-huge-pack-size-3-136ct/-/A-53551102';
 
-//   const browser = await puppeteer.launch({ headless: true });
+//   const browser = await puppeteer.launch({ headless: false });
+//   console.log(url);
 //   const page = await browser.newPage();
 //   await page.goto(url);
+//   // await page.waitForSelector(
+//   //   '.style__PriceFontSize-sc-1o3i6gc-0.kfATIS.h-text-bold'
+//   // );
 //   await page.waitForSelector(
-//     '.style__PriceFontSize-sc-1o3i6gc-0.kfATIS.h-text-bold'
+//     '.styles__CurrentPriceFontSize-sc-1mdemp3-1.dIeiFm'
 //   );
+
 //   const grabData = await page.evaluate(() => {
 //     // works for target format
 //     const formatCount = (str) => {
@@ -91,11 +97,11 @@ app.listen(port, () => {
 //       return num[0] == 0 ? (num = num.replace('0', '.')) : (num = `.${num}`);
 //     };
 //     let priceTag = document.querySelector(
-//       '.style__PriceFontSize-sc-1o3i6gc-0.kfATIS.h-text-bold'
+//       '.styles__CurrentPriceFontSize-sc-1mdemp3-1.dIeiFm'
 //     ).innerText;
-
+//     console.log('priceTag: ', priceTag);
 //     let unitPrice = document.querySelector(
-//       '.h-text-grayDark.h-text-sm.h-text-normal '
+//       '.h-text-sm.h-text-grayDark'
 //     ).innerText;
 //     unitPrice = formatCount(unitPrice);
 //     return { priceTag, unitPrice };

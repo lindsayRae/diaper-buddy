2779676e4497192f23ac2b50990585868c4cb551
Lindsay <lbarnett712@gmail.com>
2022-10-10 07:21:00 -0400

    feat: added navlink

--- client/src/pages/Error.js
+++ client/src/pages/Error.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import Navbar from '../components/Nav/Navbar';
 
 export const Error = () => {
   return (
     <>
-      <div className='error-page'>Page not found :(</div>
+      <div className='error-page'>Error: Page not found :(</div>
+      <p className='error-back'>
+        Go back <NavLink to='/home'>home</NavLink>
+      </p>
       <Navbar />
     </>
   );
